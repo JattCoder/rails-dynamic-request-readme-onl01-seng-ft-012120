e91194ae770c95fb312578da272e452b987f4668
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-04-01 16:00:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+=======
+class PostController < ApplicationController
+  def show
+>>>>>>> e106e9067737087cca68561012557eeac0c5db1b
   end
 end
